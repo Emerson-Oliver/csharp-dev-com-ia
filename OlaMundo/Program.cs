26a38658436589bb24b150eec9e5cb90f0312e26
Emerson Oliveira <emerson.520@outlook.com>
2026-09-18 17:10:36 -0300

    Aprendendo tipos primitivos

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,2 +1,32 @@
 ﻿Console.Clear();
-Console.WriteLine("Hello, World!");
+
+//Declaração estatica de variáveis
+string nome =  "Emerson";
+int idade = 41;
+
+//Declaração dinamica de variáveis
+var sobreNome = "Oliveira";
+var peso = 70.1f;
+
+Console.WriteLine("Dados do usuario: ");
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Idade: {idade}");
+Console.WriteLine($"Peso: {peso}");
+Console.WriteLine($"Nome completo: {nome} {sobreNome}");
+
+//idade = idade + peso; Ilegal pois não é possível converter implicitamente tipo "float" em "int".
+var resultado = Convert.ToString(idade) + " # " + sobreNome; //Dinamica
+string resultado2 = Convert.ToString(idade) + " # " + sobreNome; //Statica
+
+Console.WriteLine(resultado);
+Console.WriteLine(resultado2);
+Console.WriteLine("-----------------------------------");
+
+Console.WriteLine("Digite o seu nome completo: ");
+var nomeCompleto = Console.ReadLine();
+
+Console.WriteLine("Nome completo digitado: " + nomeCompleto);
+
+
+
+
--- OlaMundo/obj/Debug/net10.0/OlaMundo.AssemblyInfo.cs
+++ OlaMundo/obj/Debug/net10.0/OlaMundo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6990db74c9431c30774822071783833c6fdd34d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
